9dd70b694de7cc6b7d046e018444157906b08494
İsmail Kurt <MY_NAME@example.com>
2023-10-10 18:14:49 +0300

    Switch ifadeleri

--- Program.cs
+++ Program.cs
@@ -9,6 +9,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int ay = 10;
+
+        switch (ay)
+        {
+            case 12:
+            case 1:
+            case 2:
+                System.Console.WriteLine("kış mevsimi");
+                break;
+            case 3:
+            case 4: 
+            case 5:
+                System.Console.WriteLine("ilkbahar mevsimi");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.Console.WriteLine("yaz mevsimi");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.Console.WriteLine("sonbahar mevsimi");
+                break;
+            default:
+                System.Console.WriteLine("yanlış bilgi");
+                break;
+        }
+
     }
 }
